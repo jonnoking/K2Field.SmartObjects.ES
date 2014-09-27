438ed9a013bb373ce4323221f5b35f4fcf22f46b
jonnoking <jonno@jonnoking.com>
2014-09-26 20:22:00 -0700

    0.0.0.4

---
+++ K2Field.SmartObjects.ES.Service/K2CEPListenerService.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace K2Field.SmartObjects.ES.Service
+{
+    partial class K2CEPListenerService : ServiceBase
+    {
+
+        private System.ComponentModel.IContainer components;
+        private System.Diagnostics.EventLog eventLog1;
+
+        public K2CEPListenerService()
+        {
+            InitializeComponent();
+            eventLog1 = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("K2CEPListener"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "K2CEPListener", "K2CEPListenerLog");
+            }
+            eventLog1.Source = "K2CEPListener";
+            eventLog1.Log = "K2CEPListenerLog";
+
+            this.AutoLog = false;
+
+        }
+
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Started...");
+
+
+
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+    }
+}
